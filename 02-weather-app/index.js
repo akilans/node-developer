d28a70adb3e19e3d91006b41afaf490c7f5392ef
Akilan <akil.dove@gmail.com>
2020-09-30 22:28:26 +0100

    weather app logs improved

--- 02-weather-app/index.js
+++ 02-weather-app/index.js
@@ -1,50 +1,60 @@
 const request = require("request");
-city = encodeURIComponent(process.argv[2]);
-
-const getWeather =
-  (city,
-  () => {
-    const url = `https://www.metaweather.com/api/location/search/?query=${city}`;
-    request({ url: url, json: true }, (error, response) => {
-      if (error) {
-        console.log("Error : " + error);
-      } else {
-        let woeid = response.body[0].woeid;
-        console.log(woeid);
-        getDetails(woeid, (error, response) => {
-          if (response) {
-            const city_name = response.body.title;
-            console.log(response.body);
-            response.body.consolidated_weather.forEach((weather) => {
-              console.log(
-                `${city_name} - ${weather.applicable_date} - ${weather.the_temp} ${weather.weather_state_name}`
-              );
-            });
-          } else {
-            console.log(error);
-          }
-        });
-      }
-    });
-  });
-
-const getDetails = (woeid, callback) => {
-  const url = `https://www.metaweather.com/api/location/${woeid}`;
-  console.log(url);
+const chalk = require("chalk");
+// Get City ID
+const getCityID = (city, callback) => {
+  const url = `https://www.metaweather.com/api/location/search/?query=${city}`;
   request({ url: url, json: true }, (error, response) => {
     if (error) {
-      callback("Error Happened while getting weather report", undefined);
+      callback("Error : " + error);
+    } else {
+      if (response.body.length > 0) {
+        let woeid = response.body[0].woeid;
+        callback(undefined, woeid);
+      } else {
+        callback(`${city} - not found`, undefined);
+      }
+    }
+  });
+};
+
+// Get weather data by passing city id
+const getDetails = (woeid, callback) => {
+  const url = `https://www.metaweather.com/api/location/${woeid}`;
+  request({ url: url, json: true }, (error, response) => {
+    if (error) {
+      callback(
+        chalk.red("Error Happened while getting weather report"),
+        undefined
+      );
     } else {
       callback(undefined, response);
     }
   });
 };
 
+// Get weather report for a passing city
 if (process.argv[2]) {
-  console.log(process.argv[2]);
-  city = encodeURIComponent(process.argv[2]);
-  console.log(city);
-  getWeather(city);
+  let city_name = process.argv[2];
+  city = encodeURIComponent(city_name);
+  getCityID(city, (error, woeid) => {
+    if (error) {
+      console.log(chalk.red(error));
+    } else {
+      getDetails(woeid, (error, response) => {
+        console.log(chalk.yellow("CITY_NAME - DATE - TEMPARATURE - STATE"));
+        response.body.consolidated_weather.forEach((weather, index) => {
+          let color = index % 2 == 0 ? "green" : "blue";
+          console.log(
+            chalk[color](
+              `${city_name} - ${
+                weather.applicable_date
+              } - ${weather.the_temp.toFixed(2)}° ${weather.weather_state_name}`
+            )
+          );
+        });
+      });
+    }
+  });
 } else {
-  console.log("Please enter city name");
+  console.log(chalk.red("Please enter city name"));
 }
